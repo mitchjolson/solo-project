41447744fed106586cfc5b092f632b5e2a003d34
Mitch Olson <mitchjolson@gmail.com>
2019-08-14 16:22:57 -0500

    added date, startTime, title, and location fields to event creation page

--- src/components/EventCreate/EventCreate.js
+++ src/components/EventCreate/EventCreate.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
+
 import EventFriendsItem from '../EventFriendsItem/EventFriendsItem';
 import EventGuestsItem from '../EventGuestsItem/EventGuestsItem';
 import EventGamesItem from '../EventGamesItem/EventGamesItem';
 import EventAgendaItem from '../EventAgendaItem/EventAgendaItem';
 
 class EventCreate extends Component {
+
+    state = {
+        startDate: new Date(),
+        startTime: '',
+        location: '',
+        title: ''
+    };
 
     componentDidMount() {
         console.log('events component mounted');
@@ -19,37 +30,67 @@ class EventCreate extends Component {
         this.props.history.push('/eventcreate')
     }
 
+    handleChange = (date) => {
+        this.setState({
+            startDate: date
+        });
+        console.log('this.state is:', this.state);
+    }
+
+    handleChangeFor = (event, propToChange) => {
+        this.setState({
+            [propToChange]: event.target.value
+        })
+        console.log('this.state is:', this.state);
+    }
+
     render() {
         return (
             <>
                 <div>
-                    <h1>Create Event</h1>
+                    <p>Create Event</p>
                 </div>
                 <div>
-                    <h1>Friends</h1>
-                    <ul>
-                        {this.props.reduxStore.friends.map((friend, i) => {
-                            return (<EventFriendsItem key={i} friend={friend} history={this.props.history} />);
-                        })}
-                    </ul>
-                    <h1>Guest List</h1>
+                    <p>Guest List</p>
                     <ul>
                         {this.props.reduxStore.eventCreateGuests.map((guest, i) => {
                             return (<EventGuestsItem key={i} guest={guest} history={this.props.history} />);
                         })}
                     </ul>
-                </div>
-                <div>
-                    <h1>My Games</h1>
+                    <p>Friends</p>
                     <ul>
-                        {this.props.reduxStore.userCollection.map((game, i) => {
-                            return (<EventGamesItem key={i} game={game} history={this.props.history} />);
+                        {this.props.reduxStore.friends.map((friend, i) => {
+                            return (<EventFriendsItem key={i} friend={friend} history={this.props.history} />);
                         })}
                     </ul>
-                    <h1>Agenda</h1>
+                </div>
+                <div>
+                    <p>Event Name</p>
+                    <input type="text" value={this.state.title} placeholder='Game Night!' onChange={(event) => this.handleChangeFor(event, 'title')}/>
+                    <br/>
+                    <p>Location</p>
+                    <input type="text" value={this.state.location} onChange={(event) => this.handleChangeFor(event, 'location')} />
+                    <br />
+                    <p>Start Time</p>
+                    <input type="text" value={this.state.startTime} onChange={(event) => this.handleChangeFor(event, 'startTime')} />
+                    <br />
+                    <p>Date</p>
+                    <DatePicker
+                        selected={this.state.startDate}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <p>Agenda</p>
                     <ul>
                         {this.props.reduxStore.eventCreateGames.map((game, i) => {
                             return (<EventAgendaItem key={i} game={game} history={this.props.history} />);
+                        })}
+                    </ul>
+                    <p>My Games</p>
+                    <ul>
+                        {this.props.reduxStore.userCollection.map((game, i) => {
+                            return (<EventGamesItem key={i} game={game} history={this.props.history} />);
                         })}
                     </ul>
                 </div>
