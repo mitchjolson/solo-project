872425e3371e758628a23560680b693b65a509cc
Mitch Olson <mitchjolson@gmail.com>
2019-08-14 10:57:07 -0500

    modified nav bar links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -10,18 +10,18 @@ const Nav = (props) => (
       <h2 className="nav-title">Game Night</h2>
     </Link>
     <div className="nav-right">
-      <Link className="nav-link" to="/home">
+      <Link className="nav-link" to="/collection">
         {/* Show this link if they are logged in or not,
         but call this link 'Home' if they are logged in,
         and call this link 'Login / Register' if they are not */}
-        {props.user.id ? 'Home' : 'Login / Register'}
+        {props.user.id ? 'Collection' : 'Login / Register'}
       </Link>
       {/* Show the link to the info page and the logout button if the user is logged in */}
       {props.user.id && (
         <>
-          <Link className="nav-link" to="/collection">
+          {/* <Link className="nav-link" to="/collection">
             Collection
-          </Link>
+          </Link> */}
           <Link className="nav-link" to="/search">
             Search
           </Link>
