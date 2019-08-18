538db18fe114aa88659cb616c6814fb7898f3b52
Mitch Olson <mitchjolson@gmail.com>
2019-08-18 11:34:09 -0500

    added events page styling

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -2,6 +2,57 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EventsItem from '../EventsItem/EventsItem';
 
+// Material UI
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const CustomTableCell = withStyles(theme => ({
+    head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit,
+    },
+    input: {
+        display: 'none',
+    },
+    root: {
+        width: '80%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+        margin: "auto",
+    },
+    table: {
+        minWidth: 700,
+        align: 'center',
+    },
+    row: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.background.default,
+        },
+    },
+});
+
 class Events extends Component {
 
     componentDidMount() {
@@ -16,25 +67,26 @@ class Events extends Component {
     }
 
     render() {
+    const { classes } = this.props;
         return (
             <>
-            <div>
+            <div className="collectionDiv">
                 <h1>Events</h1>
-                    <>
-                        <table className="collectionTable">
-                            <thead>
-                                <tr><th>Event</th><th>Date</th><th>Start Time</th><th>Location</th><th>Creator</th><th>&nbsp;</th></tr>
-                            </thead>
-                            <tbody>
+                <Button variant='contained' color='primary' className={classes.button} onClick = {this.handleCreate}>Create an Event</Button>
+                    <Paper className={classes.root}>
+                        <Table className={classes.table}>
+                            <TableHead>
+                                <TableRow><CustomTableCell>Event</CustomTableCell><CustomTableCell>Date</CustomTableCell><CustomTableCell>Start Time</CustomTableCell><CustomTableCell>Location</CustomTableCell><CustomTableCell>Creator</CustomTableCell><CustomTableCell>&nbsp;</CustomTableCell></TableRow>
+                            </TableHead>
+                            <TableBody>
                                 {this.props.reduxStore.events.map((event, i) => {
                                     return (<EventsItem key={i} event={event} history={this.props.history} />);
                                 })}
-                            </tbody>
-                        </table>
-                    </>
+                            </TableBody>
+                        </Table>
+                    </Paper>
             </div>
             <div>
-                <button onClick = {this.handleCreate}>Create an Event</button>
             </div>
             </>
         )
@@ -46,4 +98,4 @@ const mapStateToProps = (reduxStore) => ({
     reduxStore
 });
 
-export default connect(mapStateToProps)(Events);
+export default connect(mapStateToProps)(withStyles(styles)(Events));
--- src/components/EventsItem/EventsItem.js
+++ src/components/EventsItem/EventsItem.js
@@ -2,8 +2,56 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Swal from 'sweetalert2';
 
-class EventsItem extends Component {
+// Material UI
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
+const CustomTableCell = withStyles(theme => ({
+    head: {
+        backgroundColor: theme.palette.common.white,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit,
+    },
+    input: {
+        display: 'none',
+    },
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 700,
+    },
+    row: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.background.default,
+        },
+    },
+});
+
+class EventsItem extends Component {
 
     handleDetails = () => {
         this.props.dispatch({ type: 'SET_EVENT_ID', payload: this.props.event.id })
@@ -11,16 +59,17 @@ class EventsItem extends Component {
     }
 
     render() {
+        const { classes } = this.props;
         return (
             <>
-                <tr>
-                    <td>{this.props.event.title}</td>
-                    <td>{this.props.event.to_char}</td>
-                    <td>{this.props.event.time}</td>
-                    <td>{this.props.event.location}</td>
-                    <td>{this.props.event.username}</td>
-                    <td><button onClick={this.handleDetails}>Details</button></td>
-                </tr>
+                <TableRow>
+                    <CustomTableCell>{this.props.event.title}</CustomTableCell>
+                    <CustomTableCell>{this.props.event.to_char}</CustomTableCell>
+                    <CustomTableCell>{this.props.event.time}</CustomTableCell>
+                    <CustomTableCell>{this.props.event.location}</CustomTableCell>
+                    <CustomTableCell>{this.props.event.username}</CustomTableCell>
+                    <CustomTableCell><Button variant='contained' size='small' onClick={this.handleDetails}>Details</Button></CustomTableCell>
+                </TableRow>
             </>
         );
     }
@@ -30,4 +79,4 @@ const mapStateToProps = (reduxStore) => ({
     reduxStore
 })
 
-export default connect(mapStateToProps)(EventsItem);
+export default connect(mapStateToProps)(withStyles(styles)(EventsItem));
